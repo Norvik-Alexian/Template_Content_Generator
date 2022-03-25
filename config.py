68a29d7560e31027fa8c799cf60358d6406bb7b7
norvikalexyan <norvik.alexyan97@protonmail.com>
2022-03-25 13:45:08 +0400

    update config file with model parameters

--- config.py
+++ config.py
@@ -2,4 +2,9 @@ import re
 
 KEYWORDS = ['Ucraft', 'drag and drop website builder', 'ecommerce', 'company']
 ENDING_PUNCTUATIONS = ['.', '...', '!']
+TEMPERATURE = [0.6, 1.]
+MAX_TOKENS = [15, 600]
+TOP_P = 1.
+FREQUENCY_PENALTY = [0.2, 1.5]
+PRESENCE_PENALTY = 0.
 space_remover = re.compile(r'\s+')
