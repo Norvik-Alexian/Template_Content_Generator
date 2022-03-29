05bb03e0e8b3567e11f0e3f70a83d8c8d3d5da62
norvikalexyan <norvik.alexyan97@protonmail.com>
2022-03-29 18:52:32 +0400

    minor update for better performance in product description module

--- product_description_generator.py
+++ product_description_generator.py
@@ -37,7 +37,7 @@ def generate_content(keywords: list):
 
 def prettify(content: str, finish_reason: str):
     try:
-        if finish_reason == 'length':
+        if finish_reason == 'stop':
             ending_punctuations = config.ENDING_PUNCTUATIONS
             any_finished_sentence = any([mark in content for mark in ending_punctuations])
             if any_finished_sentence:
@@ -67,3 +67,6 @@ def product_description_content():
     except Exception as e:
         message = f'Something went wrong with product description content, message: {e}'
         logging.error(message)
+
+
+print(product_description_content())
