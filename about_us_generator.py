3af2b195d575a9de1c46846a9f8c54340311af4f
norvikalexyan <norvik.alexyan97@protonmail.com>
2022-04-09 16:23:18 +0400

    change the aspect of the about us generator to OOP

--- about_us_generator.py
+++ about_us_generator.py
@@ -12,60 +12,68 @@ if not OPEN_API_GPT_SECRET_KEY:
     raise EnvironmentError('You should set OPEN_API_GPT_SECRET_KEY in your environment variable in .env file')
 
 
-def generate_content(keywords: list):
-    try:
-        openai.api_key = OPEN_API_GPT_SECRET_KEY
-        keywords = ' '.join(keywords)
-        model = openai.Completion.create(
-            engine='text-davinci-001',
-            prompt=f'Keywords: {keywords}\ngenerate about us content for the product:',
-            temperature=config.TEMPERATURE[1],
-            max_tokens=config.MAX_TOKENS[1],
-            top_p=config.TOP_P,
-            frequency_penalty=config.FREQUENCY_PENALTY[0],
-            presence_penalty=config.PRESENCE_PENALTY[0]
-        )
-        generated_content = model['choices'][0]['text']
+class AboutUsGenerator:
+    def __init__(self, temperature=config.TEMPERATURE[1], max_tokens=config.MAX_TOKENS[1], top_p=config.TOP_P,
+                 frequency_penalty=config.FREQUENCY_PENALTY[0], presence_penalty=config.PRESENCE_PENALTY[0]):
+        self.temperature = temperature
+        self.max_tokens = max_tokens
+        self.top_p = top_p
+        self.frequency_penalty = frequency_penalty
+        self.presence_penalty = presence_penalty
 
-        return generated_content, model
+    def generate_content(self, keywords: list):
+        try:
+            openai.api_key = OPEN_API_GPT_SECRET_KEY
+            keywords = ' '.join(keywords)
+            model = openai.Completion.create(
+                engine='text-davinci-001',
+                prompt=f'Keywords: {keywords}\ngenerate about us content for the product:',
+                temperature=self.temperature,
+                max_tokens=self.max_tokens,
+                top_p=self.top_p,
+                frequency_penalty=self.frequency_penalty,
+                presence_penalty=self.presence_penalty
+            )
+            generated_content = model['choices'][0]['text']
 
-    except Exception as e:
-        message = f'Something went wrong with generating about us content, message: {e}'
-        logging.error(message)
+            return generated_content, model
+
+        except Exception as e:
+            message = f'Something went wrong with generating about us content, message: {e}'
+            logging.error(message, exc_info=True)
+
+    def prettify(self, content: str, finish_reason: str):
+        try:
+            if finish_reason == 'stop':
+                ending_punctuations = config.ENDING_PUNCTUATIONS
+                any_finished_sentence = any([mark in content for mark in ending_punctuations])
+                if any_finished_sentence:
+                    reversed_content = content[::-1]
+                    last_finished_sentence = len(content) - 1 - min([
+                        reversed_content.index(mark) for mark in ending_punctuations if mark in content
+                    ])
+                    content = content[: last_finished_sentence + 1]
+
+            content = config.space_remover.sub(' ', content)
+
+            return content
+
+        except Exception as e:
+            message = f'Something went wrong with prettifying the content, message: {e}'
+            logging.error(message, exc_info=True)
+
+    def about_us_content(self):
+        try:
+            generated_content, model = self.generate_content(config.KEYWORDS)
+            content = generated_content.strip()
+            finish_reason = model['choices'][0]['finish_reason']
+
+            return self.prettify(content, finish_reason)
+
+        except Exception as e:
+            message = f'Something went wrong with about us content, message: {e}'
+            logging.error(message, exc_info=True)
 
 
-def prettify(content: str, finish_reason: str):
-    try:
-        if finish_reason == 'stop':
-            ending_punctuations = config.ENDING_PUNCTUATIONS
-            any_finished_sentence = any([mark in content for mark in ending_punctuations])
-            if any_finished_sentence:
-                reversed_content = content[::-1]
-                last_finished_sentence = len(content) - 1 - min([
-                    reversed_content.index(mark) for mark in ending_punctuations if mark in content
-                ])
-                content = content[: last_finished_sentence + 1]
-
-        content = config.space_remover.sub(' ', content)
-
-        return content
-
-    except Exception as e:
-        message = f'Something went wrong with prettifying the content, message: {e}'
-        logging.error(message)
-
-
-def about_us_content():
-    try:
-        generated_content, model = generate_content(config.KEYWORDS)
-        content = generated_content.strip()
-        finish_reason = model['choices'][0]['finish_reason']
-
-        return prettify(content, finish_reason)
-
-    except Exception as e:
-        message = f'Something went wrong with about us content, message: {e}'
-        logging.error(message)
-
-
-print(about_us_content())
+content = AboutUsGenerator()
+print(content.about_us_content())
