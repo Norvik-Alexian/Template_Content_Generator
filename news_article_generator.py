e4dc82ea8d9fe1fe8a008af3218e70175f788ec0
norvikalexyan <norvik.alexyan97@protonmail.com>
2022-04-09 17:35:55 +0400

    change the aspect of the news article to OOP

--- news_article_generator.py
+++ news_article_generator.py
@@ -12,61 +12,69 @@ if not OPEN_API_GPT_SECRET_KEY:
     raise EnvironmentError('You should set OPEN_API_GPT_SECRET_KEY in your environment variable in .env file')
 
 
-def generate_content(keywords: list):
-    try:
-        openai.api_key = OPEN_API_GPT_SECRET_KEY
-        keywords = ' '.join(keywords)
-        model = openai.Completion.create(
-            engine='text-davinci-001',
-            prompt=f'Keywords: {keywords}\ngenerate long news article:',
-            temperature=config.TEMPERATURE[0],
-            max_tokens=config.MAX_TOKENS[1],
-            top_p=config.TOP_P,
-            frequency_penalty=config.FREQUENCY_PENALTY[1],
-            presence_penalty=config.PRESENCE_PENALTY[1]
-        )
+class NewsArticleGenerator:
+    def __init__(self, temperature=config.TEMPERATURE[0], max_tokens=config.MAX_TOKENS[1], top_p=config.TOP_P,
+                 frequency_penalty=config.FREQUENCY_PENALTY[1], presence_penalty=config.PRESENCE_PENALTY[1]):
+        self.temperature = temperature
+        self.max_tokens = max_tokens
+        self.top_p = top_p
+        self.frequency_penalty = frequency_penalty
+        self.presence_penalty = presence_penalty
 
-        generated_content = model['choices'][0]['text']
+    def generate_content(self, keywords: list):
+        try:
+            openai.api_key = OPEN_API_GPT_SECRET_KEY
+            keywords = ' '.join(keywords)
+            model = openai.Completion.create(
+                engine='text-davinci-001',
+                prompt=f'Keywords: {keywords}\ngenerate long news article:',
+                temperature=self.temperature,
+                max_tokens=self.max_tokens,
+                top_p=self.top_p,
+                frequency_penalty=self.frequency_penalty,
+                presence_penalty=self.presence_penalty
+            )
 
-        return generated_content, model
+            generated_content = model['choices'][0]['text']
 
-    except Exception as e:
-        message = f'Something went wrong with generating news article content, message: {e}'
-        logging.error(message)
+            return generated_content, model
+
+        except Exception as e:
+            message = f'Something went wrong with generating news article content, message: {e}'
+            logging.error(message, exc_info=True)
+
+    def prettify(self, content: str, finish_season: str):
+        try:
+            if finish_season == 'stop':
+                ending_punctuations = config.ENDING_PUNCTUATIONS
+                any_finished_sentence = any([mark in content for mark in ending_punctuations])
+                if any_finished_sentence:
+                    reversed_content = content[::-1]
+                    last_finished_sentence = len(content) - 1 - min([
+                        reversed_content.index(mark) for mark in ending_punctuations if mark in content
+                    ])
+                    content = content[: last_finished_sentence + 1]
+
+            content = config.space_remover.sub(' ', content)
+
+            return content
+
+        except Exception as e:
+            message = f'Something went wrong with prettifying the generated content, message: {e}'
+            logging.error(message, exc_info=True)
+
+    def news_article_content(self):
+        try:
+            generated_content, model = self.generate_content(config.KEYWORDS)
+            content = generated_content.strip()
+            finish_reason = model['choices'][0]['finish_reason']
+
+            return self.prettify(content, finish_reason)
+
+        except Exception as e:
+            message = f'Something went wrong the news article content, message: {e}'
+            logging.error(message, exc_info=True)
 
 
-def prettify(content: str, finish_season: str):
-    try:
-        if finish_season == 'stop':
-            ending_punctuations = config.ENDING_PUNCTUATIONS
-            any_finished_sentence = any([mark in content for mark in ending_punctuations])
-            if any_finished_sentence:
-                reversed_content = content[::-1]
-                last_finished_sentence = len(content) - 1 - min([
-                    reversed_content.index(mark) for mark in ending_punctuations if mark in content
-                ])
-                content = content[: last_finished_sentence + 1]
-
-        content = config.space_remover.sub(' ', content)
-
-        return content
-
-    except Exception as e:
-        message = f'Something went wrong with prettifying the generated content, message: {e}'
-        logging.error(message)
-
-
-def news_article_content():
-    try:
-        generated_content, model = generate_content(config.KEYWORDS)
-        content = generated_content.strip()
-        finish_reason = model['choices'][0]['finish_reason']
-
-        return prettify(content, finish_reason)
-
-    except Exception as e:
-        message = f'Something went wrong the news article content, message: {e}'
-        logging.error(message)
-
-
-print(news_article_content())
+content = NewsArticleGenerator()
+print(content.news_article_content())
