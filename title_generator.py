98b56efb9d89f074e0262efb8971725eac82a2de
norvikalexyan <norvik.alexyan97@protonmail.com>
2022-04-09 19:55:35 +0400

    change the aspect of the title generator to OOP

--- title_generator.py
+++ title_generator.py
@@ -12,29 +12,39 @@ if not OPEN_API_GPT_SECRET_KEY:
     raise EnvironmentError('You should set OPEN_API_GPT_SECRET_KEY in your environment variable in .env file')
 
 
-def generate_title(keywords: list):
-    try:
-        openai.api_key = OPEN_API_GPT_SECRET_KEY
-        keywords = ' '.join(keywords)
-        model = openai.Completion.create(
-            engine='text-davinci-001',
-            prompt=f'Keywords: {keywords}\ngenerate short headline for the content:',
-            temperature=config.TEMPERATURE[1],
-            max_tokens=config.MAX_TOKENS[0],
-            top_p=config.TOP_P,
-            frequency_penalty=config.FREQUENCY_PENALTY[2],
-            presence_penalty=config.PRESENCE_PENALTY[0]
-        )
+class TitleGenerator:
+    def __init__(self, temperature=config.TEMPERATURE[1], max_tokens=config.MAX_TOKENS[0], top_p=config.TOP_P,
+                 frequency_penalty=config.FREQUENCY_PENALTY[2], presence_penalty=config.PRESENCE_PENALTY[0]):
+        self.temperature = temperature
+        self.max_tokens = max_tokens
+        self.top_p = top_p
+        self.frequency_penalty = frequency_penalty
+        self.presence_penalty = presence_penalty
 
-        headline = model['choices'][0]['text'].strip()
-        headline = headline.strip('Re:').strip()
-        headline = config.space_remover.sub(' ', headline)
+    def generate_title(self, keywords: list):
+        try:
+            openai.api_key = OPEN_API_GPT_SECRET_KEY
+            keywords = ' '.join(keywords)
+            model = openai.Completion.create(
+                engine='text-davinci-001',
+                prompt=f'Keywords: {keywords}\ngenerate short headline for the content:',
+                temperature=self.temperature,
+                max_tokens=self.max_tokens,
+                top_p=self.top_p,
+                frequency_penalty=self.frequency_penalty,
+                presence_penalty=self.presence_penalty
+            )
 
-        return headline
+            headline = model['choices'][0]['text'].strip()
+            headline = headline.strip('Re:').strip()
+            headline = config.space_remover.sub(' ', headline)
 
-    except Exception as e:
-        message = f'Something went wrong with generating titles, message: {e}'
-        logging.error(message)
+            return headline
+
+        except Exception as e:
+            message = f'Something went wrong with generating titles, message: {e}'
+            logging.error(message, exc_info=True)
 
 
-print(generate_title(config.KEYWORDS))
+content = TitleGenerator()
+print(content.generate_title(config.KEYWORDS))
