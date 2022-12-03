dffa22c000455d6f80bfb231b67fb5e7c744f0be
Norvik-Alexian <norvik.alexyan97@protonmail.com>
2022-12-03 23:17:46 +0400

    update max tokens value

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import re
 KEYWORDS = ['Ucraft', 'drag and drop website builder', 'ecommerce', 'company']
 ENDING_PUNCTUATIONS = ['.', '!']
 TEMPERATURE = 0.7
-MAX_TOKENS = [15, 80]
+MAX_TOKENS = 80
 TOP_P = 1.
 FREQUENCY_PENALTY = 1.
 PRESENCE_PENALTY = 1.
