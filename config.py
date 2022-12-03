3e4dd19179fc715933f4ac52dae170d6cbcd18b8
Norvik-Alexian <norvik.alexyan97@protonmail.com>
2022-12-03 21:48:33 +0400

    update values of identifiers

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import re
 
 KEYWORDS = ['Ucraft', 'drag and drop website builder', 'ecommerce', 'company']
 ENDING_PUNCTUATIONS = ['.', '!']
-TEMPERATURE = [0.6, 1.]
+TEMPERATURE = 0.7
 MAX_TOKENS = [15, 80]
 TOP_P = 1.
-FREQUENCY_PENALTY = [0.2, 0.3, 1.5]
-PRESENCE_PENALTY = [0., 0.1]
+FREQUENCY_PENALTY = 1.
+PRESENCE_PENALTY = 1.
 SPACE_REMOVER_PATTERN = re.compile(r'\s+')
