6ccb0444025854b0f89c79e438b623ea2314be98
Norvik-Ucraft <norvik@ucraft.com>
2022-12-03 20:56:20 +0400

    change some of the values of identifiers

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import re
 
 KEYWORDS = ['Ucraft', 'drag and drop website builder', 'ecommerce', 'company']
-ENDING_PUNCTUATIONS = ['.', '...', '!']
+ENDING_PUNCTUATIONS = ['.', '!']
 TEMPERATURE = [0.6, 1.]
-MAX_TOKENS = [15, 600]
+MAX_TOKENS = [15, 80]
 TOP_P = 1.
 FREQUENCY_PENALTY = [0.2, 0.3, 1.5]
 PRESENCE_PENALTY = [0., 0.1]
-space_remover = re.compile(r'\s+')
+SPACE_REMOVER_PATTERN = re.compile(r'\s+')
