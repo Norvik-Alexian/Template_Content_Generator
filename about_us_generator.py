8ec74d17aeac2f162ebfe29244b1a7db63d7a290
Norvik-Ucraft <norvik@ucraft.com>
2022-12-03 20:59:02 +0400

    refactoring about us generator module code

--- about_us_generator.py
+++ about_us_generator.py
@@ -4,6 +4,7 @@ import config
 import openai
 
 from dotenv import load_dotenv
+from utils import prettify
 
 load_dotenv()
 OPEN_API_GPT_SECRET_KEY = os.getenv('OPEN_API_GPT_SECRET_KEY')
@@ -13,68 +14,40 @@ if not OPEN_API_GPT_SECRET_KEY:
 
 
 class AboutUsGenerator:
-    def __init__(self, temperature=config.TEMPERATURE[1], max_tokens=config.MAX_TOKENS[1], top_p=config.TOP_P,
-                 frequency_penalty=config.FREQUENCY_PENALTY[0], presence_penalty=config.PRESENCE_PENALTY[0]):
+    def __init__(self,
+                 temperature=config.TEMPERATURE,
+                 max_tokens=config.MAX_TOKENS,
+                 top_p=config.TOP_P,
+                 frequency_penalty=config.FREQUENCY_PENALTY,
+                 presence_penalty=config.PRESENCE_PENALTY):
         self.temperature = temperature
         self.max_tokens = max_tokens
         self.top_p = top_p
         self.frequency_penalty = frequency_penalty
         self.presence_penalty = presence_penalty
 
-    def generate_content(self, keywords: list):
+    @property
+    def generate_content(self):
         try:
             openai.api_key = OPEN_API_GPT_SECRET_KEY
-            keywords = ' '.join(keywords)
+            keywords = ' '.join(config.KEYWORDS)
             model = openai.Completion.create(
-                engine='text-davinci-001',
+                engine='text-davinci-002',
                 prompt=f'Keywords: {keywords}\ngenerate about us content for the product:',
-                temperature=self.temperature,
-                max_tokens=self.max_tokens,
+                temperature=self.temperature[1],
+                max_tokens=self.max_tokens[1],
                 top_p=self.top_p,
-                frequency_penalty=self.frequency_penalty,
-                presence_penalty=self.presence_penalty
+                frequency_penalty=self.frequency_penalty[0],
+                presence_penalty=self.presence_penalty[0]
             )
-            generated_content = model['choices'][0]['text']
+            generated_content = model['choices'][0]['text'].strip()
+            finish_reason = model['choices'][0]['finish_reason']
 
-            return generated_content, model
-
+            return prettify(generated_content, finish_reason)
         except Exception as e:
             message = f'Something went wrong with generating about us content, message: {e}'
             logging.error(message, exc_info=True)
 
-    @staticmethod
-    def prettify(content: str, finish_reason: str):
-        try:
-            if finish_reason == 'stop':
-                ending_punctuations = config.ENDING_PUNCTUATIONS
-                any_finished_sentence = any([mark in content for mark in ending_punctuations])
-                if any_finished_sentence:
-                    reversed_content = content[::-1]
-                    last_finished_sentence = len(content) - 1 - min([
-                        reversed_content.index(mark) for mark in ending_punctuations if mark in content
-                    ])
-                    content = content[: last_finished_sentence + 1]
-
-            content = config.space_remover.sub(' ', content)
-
-            return content
-
-        except Exception as e:
-            message = f'Something went wrong with prettifying the content, message: {e}'
-            logging.error(message, exc_info=True)
-
-    def about_us_content(self):
-        try:
-            generated_content, model = self.generate_content(config.KEYWORDS)
-            content = generated_content.strip()
-            finish_reason = model['choices'][0]['finish_reason']
-
-            return self.prettify(content, finish_reason)
-
-        except Exception as e:
-            message = f'Something went wrong with about us content, message: {e}'
-            logging.error(message, exc_info=True)
-
 
 content = AboutUsGenerator()
-print(content.about_us_content())
+print(content.generate_content)
