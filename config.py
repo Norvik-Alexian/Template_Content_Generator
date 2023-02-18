b419197f2d4c17edae7d62791fd416887a6a553e
Norvik-Alexian <norvik.alexyan97@protonmail.com>
2023-02-18 15:15:53 +0400

    added about us prompt sample

--- config.py
+++ config.py
@@ -9,3 +9,6 @@ TOP_P = 1.
 FREQUENCY_PENALTY = 1.
 PRESENCE_PENALTY = 1.
 N = 1
+ABOUT_US_PROMPT = f'''Write an "About Us" content in more than 2 paragraphs within the Keywords and include company
+mission and purpose, establish a mission statement, explain who you serve, explain what you're offering and what makes
+you unique, describe your values, direct customer towards the next action:'''
