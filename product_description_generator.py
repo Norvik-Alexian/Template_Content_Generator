c608e8467148c2e77b7eb7bd19490a8fa5f4968b
Norvik-Alexian <norvik.alexyan97@protonmail.com>
2023-02-18 15:24:39 +0400

    minor updates in the product description generators code for better performance

--- product_description_generator.py
+++ product_description_generator.py
@@ -7,48 +7,48 @@ from utils import prettify
 from dotenv import load_dotenv
 
 load_dotenv()
-OPEN_API_GPT_SECRET_KEY = os.getenv('OPEN_API_GPT_SECRET_KEY')
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
-if not OPEN_API_GPT_SECRET_KEY:
-    raise EnvironmentError('You should set OPEN_API_GPT_SECRET_KEY in your environment variable in .env file')
+if not OPENAI_API_KEY:
+    raise EnvironmentError('You should set OPENAI_API_KEY in your environment variable in .env file')
 
 
 class ProductDescriptionGenerator:
+    openai.api_key = OPENAI_API_KEY
+
     def __init__(self,
+                 engine=config.ENGINE,
                  temperature=config.TEMPERATURE,
                  max_tokens=config.MAX_TOKENS,
                  top_p=config.TOP_P,
+                 n=config.N,
                  frequency_penalty=config.FREQUENCY_PENALTY,
                  presence_penalty=config.PRESENCE_PENALTY):
+        self.engine = engine
         self.temperature = temperature
         self.max_tokens = max_tokens
         self.top_p = top_p
+        self.n = n
         self.frequency_penalty = frequency_penalty
         self.presence_penalty = presence_penalty
 
-    @property
-    def generate_product_description(self):
+    def generate_product_description(self, keywords: list):
         try:
-            openai.api_key = OPEN_API_GPT_SECRET_KEY
-            keywords = ' '.join(config.KEYWORDS)
             model = openai.Completion.create(
-                engine='text-davinci-002',
+                engine=self.engine,
                 prompt=f'Generate Product Description within the Keywords\nKeywords: {keywords}\nProduct Description:',
                 temperature=self.temperature,
                 max_tokens=self.max_tokens,
                 top_p=self.top_p,
+                n=self.n,
                 frequency_penalty=self.frequency_penalty,
                 presence_penalty=self.presence_penalty
             )
 
-            product_description = model['choices'][0]['text'].strip()
-            finish_reason = model['choices'][0]['finish_reason']
+            product_description = model.choices[0].text.strip()
+            finish_reason = model.choices[0].finish_reason
 
             return prettify(product_description, finish_reason)
         except Exception as e:
             message = f'Something went wrong with generating product description, message: {e}'
             logging.error(message, exc_info=True)
-
-
-content = ProductDescriptionGenerator()
-print(content.generate_product_description)
