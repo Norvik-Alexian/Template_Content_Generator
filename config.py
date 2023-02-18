c6414e04acc1e822c2c68f7bcd1b71e68b435cb8
Norvik-Alexian <norvik.alexyan97@protonmail.com>
2023-02-18 14:46:32 +0400

    update the engine of the model and add one new hard coded parameter in the config module

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 import re
 
-KEYWORDS = ['Ucraft', 'drag and drop website builder', 'ecommerce', 'company']
 ENDING_PUNCTUATIONS = ['.', '!']
+SPACE_REMOVER_PATTERN = re.compile(r'\s+')
+ENGINE = 'text-davinci-003'
 TEMPERATURE = 0.7
 MAX_TOKENS = 80
 TOP_P = 1.
 FREQUENCY_PENALTY = 1.
 PRESENCE_PENALTY = 1.
-SPACE_REMOVER_PATTERN = re.compile(r'\s+')
+N = 1
