a67bfa85a58e095d406bf3541d0e7d879901e330
Norvik-Alexian <norvik.alexyan97@protonmail.com>
2023-02-18 16:57:49 +0400

    added docstring to content prettifier function

--- utils.py
+++ utils.py
@@ -2,6 +2,11 @@ import config
 
 
 def prettify(content: str, finish_reason: str):
+    """
+    :param content: generated content that has unfinished sequence
+    :param finish_reason: one of the models output that shows if content has unfinished sequence or not
+    :return: generated content that has no unfinished sequence
+    """
     if finish_reason == 'length':
         ending_punctuations = config.ENDING_PUNCTUATIONS
         any_finished_sequence = any([mark in content for mark in ending_punctuations])
@@ -11,7 +16,6 @@ def prettify(content: str, finish_reason: str):
                 [reversed_content.index(mark) for mark in ending_punctuations if mark in content]
             )
             content = content[:last_finished_sequence + 1]
-
     content = config.SPACE_REMOVER_PATTERN.sub(' ', content)
 
     return content
