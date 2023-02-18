715b598d392a0f7ecfbaa12b12cf86cbcbcaf27d
Norvik-Alexian <norvik.alexyan97@protonmail.com>
2023-02-18 15:02:42 +0400

    minor updates in the code

--- title_generator.py
+++ title_generator.py
@@ -7,48 +7,48 @@ from utils import prettify
 from dotenv import load_dotenv
 
 load_dotenv()
-OPEN_API_GPT_SECRET_KEY = os.getenv('OPEN_API_GPT_SECRET_KEY')
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
-if not OPEN_API_GPT_SECRET_KEY:
-    raise EnvironmentError('You should set OPEN_API_GPT_SECRET_KEY in your environment variable in .env file')
+if not OPENAI_API_KEY:
+    raise EnvironmentError('You should set OPENAI_API_KEY in your environment variable in .env file')
 
 
 class TitleGenerator:
+    openai.api_key = OPENAI_API_KEY
+
     def __init__(self,
+                 engine=config.ENGINE,
                  temperature=config.TEMPERATURE,
                  max_tokens=config.MAX_TOKENS,
                  top_p=config.TOP_P,
+                 n=config.N,
                  frequency_penalty=config.FREQUENCY_PENALTY,
                  presence_penalty=config.PRESENCE_PENALTY):
+        self.engine = engine
         self.temperature = temperature
         self.max_tokens = max_tokens
         self.top_p = top_p
+        self.n = n
         self.frequency_penalty = frequency_penalty
         self.presence_penalty = presence_penalty
 
-    @property
-    def generate_title(self):
+    def generate_title(self, keywords: list):
         try:
-            openai.api_key = OPEN_API_GPT_SECRET_KEY
-            keywords = ' '.join(config.KEYWORDS)
             model = openai.Completion.create(
-                engine='text-davinci-002',
+                engine=self.engine,
                 prompt=f'Generate Article Title within the Keywords\nKeywords: {keywords}\nArticle Title:',
                 temperature=self.temperature,
                 max_tokens=self.max_tokens,
                 top_p=self.top_p,
+                n=self.n,
                 frequency_penalty=self.frequency_penalty,
                 presence_penalty=self.presence_penalty
             )
 
-            title = model['choices'][0]['text'].strip()
-            finish_reason = model['choices'][0]['finish_reason']
+            title = model.choices[0].text.strip()
+            finish_reason = model.choices[0].finish_reason
 
             return prettify(title, finish_reason)
         except Exception as e:
             message = f'Something went wrong with generating titles, message: {e}'
             logging.error(message, exc_info=True)
-
-
-content = TitleGenerator()
-print(content.generate_title)
