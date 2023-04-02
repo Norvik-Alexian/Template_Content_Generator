4bd2a7774c8bf5d9581e89a3565eb13d7f05b5f2
Norvik-Alexian <norvik.alexyan97@protonmail.com>
2023-04-02 21:41:40 +0400

    update the model of openai and added new identifier which is model's parameter

--- config.py
+++ config.py
@@ -2,13 +2,14 @@ import re
 
 ENDING_PUNCTUATIONS = ['.', '!']
 SPACE_REMOVER_PATTERN = re.compile(r'\s+')
-ENGINE = 'text-davinci-003'
+MODEL = 'gpt-3.5-turbo	'
 TEMPERATURE = 0.7
 MAX_TOKENS = 80
 TOP_P = 1.
 FREQUENCY_PENALTY = 1.
 PRESENCE_PENALTY = 1.
 N = 1
+STOP = None
 ABOUT_US_PROMPT = f'''Write an "About Us" content in more than 2 paragraphs within the Keywords and include company
 mission and purpose, establish a mission statement, explain who you serve, explain what you're offering and what makes
 you unique, describe your values, direct customer towards the next action:'''
