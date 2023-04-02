fdaab2863b0d3a74638ce9a25f9335491d23caed
Norvik-Alexian <norvik.alexyan97@protonmail.com>
2023-04-02 21:42:26 +0400

    update the model of openai change some parameters of the model and update the class attributes

--- about_us_generator.py
+++ about_us_generator.py
@@ -3,6 +3,7 @@ import os
 import config
 import openai
 
+from openai.error import APIError
 from dotenv import load_dotenv
 from utils import prettify
 
@@ -17,20 +18,22 @@ class AboutUsGenerator:
     openai.api_key = OPENAI_API_KEY
 
     def __init__(self,
-                 engine=config.ENGINE,
+                 model=config.MODEL,
                  prompt=config.ABOUT_US_PROMPT,
                  temperature=config.TEMPERATURE,
                  max_tokens=config.MAX_TOKENS,
                  top_p=config.TOP_P,
                  n=config.N,
+                 stop=config.STOP,
                  frequency_penalty=config.FREQUENCY_PENALTY,
                  presence_penalty=config.PRESENCE_PENALTY):
-        self.engine = engine
+        self.model = model
         self.prompt = prompt
         self.temperature = temperature
         self.max_tokens = max_tokens
         self.top_p = top_p
         self.n = n
+        self.stop = stop
         self.frequency_penalty = frequency_penalty
         self.presence_penalty = presence_penalty
 
@@ -40,20 +43,24 @@ class AboutUsGenerator:
         :return: generated about us content that went through content prettifier
         """
         try:
-            model = openai.Completion.create(
-                engine=self.engine,
-                prompt=f'{self.prompt}\nKeywords: {keywords}\nAbout Us:',
+            model = openai.ChatCompletion.create(
+                model=self.model,
+                messages=[
+                    {'role': 'system', 'content': 'You are About Us generator that generates the content based on the keywords'},
+                    {'role': 'user', 'content': f'{self.prompt}\nKeywords: {keywords}\nAbout Us:'}
+                ],
                 temperature=self.temperature,
                 max_tokens=self.max_tokens,
                 top_p=self.top_p,
                 n=self.n,
+                stop=self.stop,
                 frequency_penalty=self.frequency_penalty,
                 presence_penalty=self.presence_penalty
             )
-            generated_content = model.choices[0].text.strip()
+            generated_content = model.choices[0].message.content.strip()
             finish_reason = model.choices[0].finish_reason
 
             return prettify(generated_content, finish_reason)
-        except Exception as e:
-            message = f'Something went wrong with generating about us content, message: {e}'
+        except APIError as e:
+            message = f'OpenAI model has outage, message: {e}'
             logging.error(message, exc_info=True)
